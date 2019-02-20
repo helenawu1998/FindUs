46119406dd962dffb17ec9642933efbf6406e740
Pamela Zhang <pqzhang@caltech.edu>
2019-02-20 12:28:34 -0800

    Add Map Screen.
    A real map has now been added to the first page.
    The next step is to add markers in certain
    cities based on database info.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,45 +1,64 @@
 import React from 'react';
-import { Image, ScrollView, StyleSheet, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, View, } from 'react-native';
+import { MapView } from 'expo';
 import { SearchBar } from 'react-native-elements';
+//import { Marker } from 'react-native-maps';
 
-export default class MapScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Map',
-  };
+// export default class MapScreen extends React.Component {
+//   static navigationOptions = {
+//     title: 'Map',
+//   };
 
-  state = {
-    search: '',
-  };
+//   state = {
+//     search: '',
+//   };
 
-  updateSearch = search => {
-    this.setState({ search });
-  };
+//   updateSearch = search => {
+//     this.setState({ search });
+//   };
 
+//   render() {
+//     const { search } = this.state;
+
+//     return (
+//       <ScrollView style={styles.container}>
+//         <SearchBar platform="ios" placeholder="Search" onChangeText={this.updateSearch} value={search}/>
+//         <View style={styles.mapContainer}>
+//           <Image source={require('../assets/images/map.png')} style={styles.mapImage}/>
+//         </View>
+//       </ScrollView>
+//     );
+//   }
+// }
+
+export default class App extends React.Component {
   render() {
-    const { search } = this.state;
-
     return (
-      <ScrollView style={styles.container}>
-        <SearchBar platform="ios" placeholder="Search" onChangeText={this.updateSearch} value={search}/>
-        <View style={styles.mapContainer}>
-          <Image source={require('../assets/images/map.png')} style={styles.mapImage}/>
-        </View>
-      </ScrollView>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 118.78825,
+          longitude: 34.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  mapContainer: {
-  },
-  mapImage: {
-    width: 480,
-    height: 850,
-    resizeMode: 'contain',
-  },
-});
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+//   mapContainer: {
+//   },
+//   mapImage: {
+//     width: 480,
+//     height: 850,
+//     resizeMode: 'contain',
+//   },
+// });
 
