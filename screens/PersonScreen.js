1376ddff6e69569c8d9ca51954cc8e53548072cd
Sharon Chen <stchen@caltech.edu>
2019-03-03 21:57:04 -0800

    Update person screen

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -21,6 +21,7 @@ export default class PersonScreen extends React.Component {
     sex: '',
     race: '',
     date_mod: '',
+    imgUrl: '',
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -32,12 +33,23 @@ export default class PersonScreen extends React.Component {
   componentWillMount() {
     this.state.casenum = this.props.navigation.state.params.casenum;
     // TODO: fetch case data from database!
+    this.state.DLC = 'DLC';
+    this.state.last_name = 'Wu';
+    this.state.first_name = 'Helena';
+    this.state.miss_age = '20';
+    this.state.city = 'Pasadena';
+    this.state.county = 'Los Angeles';
+    this.state.state = 'CA';
+    this.state.sex = 'F';
+    this.state.race = 'Chinese';
+    this.state.date_mod = 'Feb 28, 2019';
+    this.state.imgUrl = '../assets/images/person-icon.png';
   }
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        <Text style={styles.title}>Case #{this.state.casenum}</Text>
+        <Text style={styles.title}>{this.state.first_name} {this.state.last_name}</Text>
       </ScrollView>
     );
   }
@@ -47,10 +59,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    margin: 10,
+    margin: 15,
   },
   title: {
-    margin: 15,
     fontSize: 20,
     fontWeight: 'bold',
   },
